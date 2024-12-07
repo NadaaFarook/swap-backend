f3f662bc3f1fbb8515fe223e577e3e3ec96de1ed
chann44 <44chansw@gmail.com>
2024-12-07 20:15:18 +0530

    added price api to server

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,17 @@ import dotenv from "dotenv";
 import App from "./server";
 import { PoolHandler } from "./handlers/pool-handler";
 import { TxHandler } from "./handlers/tx-handler";
+import { PriceHandler } from "./handlers/price-handler";
 
 dotenv.config({ path: process.env.DOTENV_CONFIG_PATH });
 
 const router = Router();
 
-const routes: any[] = [new PoolHandler(router), new TxHandler(router)];
+const routes: any[] = [
+    new PoolHandler(router),
+    new TxHandler(router),
+    new PriceHandler(router),
+];
 
 const app = new App(routes);
 app.listen();
